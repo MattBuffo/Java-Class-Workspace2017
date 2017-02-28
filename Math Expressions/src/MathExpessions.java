76df927dde42e15340e782dd6e3431a8021dd7f8
MattBuffo <mattthel33t@gmail.com>
2017-02-28 17:23:33 -0500

    Finished change making program

---
+++ Math Expressions/src/MathExpessions.java
@@ -0,0 +1,12 @@
+
+public class MathExpessions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double b = 2;
+		double a = 3;
+		double z = Math.abs(b - a);
+		System.out.println(z);
+	}
+
+}
